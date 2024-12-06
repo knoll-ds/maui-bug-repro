eb008985c01d50b28719edfc067c7e84578ef0b1
dennis.schmidt <dennis.schmidt@knoll-mb.de>
2024-12-06 08:48:32 +0100

    Bug repro added

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnClicked(object sender, EventArgs e)
+	{
+		await Shell.Current.GoToAsync("//SecondPage");
+	}
+}
+
